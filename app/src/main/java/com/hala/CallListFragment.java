b9b268b990fcc3ad9b655dc134858aad0b5e20a0
zhougangwei <892537848@qq.com>
2019-05-26 12:04:07 +0800

    sdfsdf sadgsad

---
+++ app/src/main/java/com/hala/CallListFragment.java
@@ -0,0 +1,16 @@
+package com.hala;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class CallListFragment  extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+}
