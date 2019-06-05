90b077b9c519abf8969b8a234aafa5fd52313f6e
zhougangwei <892537848@qq.com>
2019-06-05 09:25:53 +0800

    first

---
+++ app/src/main/java/com/hala/activity/EditProActivity.java
@@ -0,0 +1,25 @@
+package com.hala.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hala.R;
+import com.hala.base.BaseActivity;
+
+public class EditProActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_edit_pro;
+    }
+
+    @Override
+    protected void beforeInitView() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
